16f66ce5d9ca745a68cfd18d36b6f7c3f3447cb9
Coco Puff <fahrija_wyroski@live.com>
2021-08-08 12:45:54 -0500

    add about section and start defining in accordance ot header elements

---
+++ private-project-fw/src/components/About/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Who Are We?</h1>
+            <div className="my-2">
+                <p>
+                    This section is About the Company, with short intro and services offered.
+        </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
